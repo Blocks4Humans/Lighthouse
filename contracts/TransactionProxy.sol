65ae06342d277b780d430af949c898e533b7e396
Ricardo Rius <ricardorius@live.com.mx>
2018-11-04 19:20:27 -0600

    *gitattributes

--- contracts/TransactionProxy.sol
+++ contracts/TransactionProxy.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.4.24;
 
+import "./libraries/openzeppelin/migrations/Initializable.sol";
 import "./libraries/openzeppelin/token/ERC20/ERC20Basic.sol";
 import "./libraries/openzeppelin/ECRecovery.sol";
 import "./libraries/uport/Proxy.sol";
@@ -10,16 +11,22 @@ contract TransactionProxy is Proxy {
 
     using ECRecovery for bytes32;
 
+    // STATE VARIABLES
     address ethrReg;
     mapping(address => uint) public nonce;
     mapping(address => bool) public whitelist;
     
-    constructor(address registry) public {
+    function initialize(address _ethrReg) external isInitializer {
         whitelist[msg.sender] = true;
-        ethrReg = registry;
+        ethrReg = _ethrReg;
     }
 
-    event UpdateWhitelist(address _account, bool _value);
+    // EVENTS
+    event UpdateWhitelist(
+        address account, 
+        bool value
+	);
+
     event Forwarded (
         bytes sig, 
         address signer, 
@@ -28,9 +35,10 @@ contract TransactionProxy is Proxy {
         bytes data,
         address rewardToken, 
         uint rewardAmount,
-        bytes32 _hash
+        bytes32 hash
     );
 
+    // MODIFIERS
     /// @dev Verifies if caller is whitelisted.
     modifier ifWhitelisted() {
         if(whitelist[msg.sender] == true) {
@@ -40,13 +48,17 @@ contract TransactionProxy is Proxy {
         }
     }
 
+    /*
+    PUBLIC FUNCTIONS
+    */
     function updateWhitelist(address _account) public ifWhitelisted returns(bool) {
         //Validate signer delegate from Ethr registry. Secp256k1VerificationKey2018 -> "veriKey"
+
         bool result = isValidDelegate(msg.sender, Bytes.stringToBytes32("veriKey"), _account); 
         require(result == true, ", not a valid ethr signer delegate.");
         whitelist[_account] = result;
         emit UpdateWhitelist(_account,result);
-        return true;
+        return result;
     }
 
     function forward(
@@ -79,33 +91,35 @@ contract TransactionProxy is Proxy {
         emit Forwarded(sig, signer, destination, value, data, rewardToken, rewardAmount, _hash);
     }
 
+    /*
+    INTERNAL FUNCTIONS
+    */
     /// @dev Verifies if the signer delegate is valid in the Ethr DID registry.
-    /// @dev Similar to registry.call(bytes4(keccak256("validDelegate(address,bytes32,address)")), abi.encode(_identity, _delegateType, _delegate))
+    /// @dev ethrReg The address of the registry. Needs to be a storate variable in the contract.
     /// @param _identity The address owner.
     /// @param _delegateType Type of delegate. Signer -> Secp256k1VerificationKey2018 -> bytes32("veriKey").
     /// @param _delegate The address of the signer delegate.
     function isValidDelegate(address _identity, bytes32 _delegateType, address _delegate) internal ifWhitelisted returns(bool result){
         require(ethrReg != address(0), ", ethr registry not set.");
         require(_identity != address(0) && _delegateType != bytes32(0) && _delegate != address(0),", invalid delegate input.");
-        bytes4 funcSig = bytes4(keccak256("validDelegate(address,bytes32,address)"));
+        bytes4 VALID_DELEG_FSIG = bytes4(keccak256("validDelegate(address,bytes32,address)"));
         /* solium-disable-next-line security/no-inline-assembly */
         assembly {
             // Move pointer to free memory spot.
             let ptr := mload(0x40)
             // Put function signature at memory spot.
-            mstore(ptr,funcSig)
+            mstore(ptr,VALID_DELEG_FSIG)
             // Append arguments after function signature.
             mstore(add(ptr,0x04), _identity)
             mstore(add(ptr,0x24), _delegateType)
             mstore(add(ptr,0x44), _delegate)
-            let success := call(
-              4000, // Gas limit.
+            let success := staticcall(
+              3800, // Gas limit.
               sload(ethrReg_slot), // Append _slot to access Ethr DID registry storage.
-              0, // No ether tansfer.
-              ptr, // Inputs are stored at location ptr.
+              ptr,  // Inputs are stored at location ptr.
               0x64, // Sum of all inputs is 100 bytes long.
-              ptr,  //Store output over input.
-              0x20) //Outputs are 32 bytes long.
+              ptr,  // Store output over input.
+              0x20) // Outputs are 32 bytes long.
             if eq(success, 0) {
                 revert(0, 0)
             }
